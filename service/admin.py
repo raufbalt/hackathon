10cb2efec9d8f840a715bd06301913127ecce65c
raufbalt <baltabaevrauf9@gmail.com>
2022-11-04 22:26:54 +0600

    deleted subcategory

--- service/admin.py
+++ service/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from service.models import Service, Category, Subcategory
+from service.models import Service, Category
 
 admin.site.register(Service)
 admin.site.register(Category)
-admin.site.register(Subcategory)
