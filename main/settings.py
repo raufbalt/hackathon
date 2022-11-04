467a80242526c66435491859099d0100accfe5f5
raufbalt <baltabaevrauf9@gmail.com>
2022-11-04 22:12:48 +0600

    added new external ip

--- main/settings.py
+++ main/settings.py
@@ -196,7 +196,7 @@ CORS_ALLOW_ALL_ORIGINS =True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://35.222.42.99",
+    "35.222.42.99",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
