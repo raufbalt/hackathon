5fdc13a2b11cd1e41c7471ce9d752e305ba0c2c0
NurgulDND <aliniki2005@gmail.com>
2022-11-07 14:55:59 +0600

    reviews

--- account/views.py
+++ account/views.py
@@ -30,11 +30,11 @@ class ActivationView(APIView):
 
     def get(self, request, activation_code):
         try:
-            user = User.objects.get(activation_code = activation_code)
+            user = User.objects.get(activation_code=activation_code)
             user.is_active = True
             user.activation_code = ''
             user.save()
-            return  Response({'msg':'Successfully activated'}, status=200)
+            return Response({'msg':'Successfully activated'}, status=200)
         except User.DoesNotExist:
             return Response({'msg:', 'Link expired!'}, status=400)
 
